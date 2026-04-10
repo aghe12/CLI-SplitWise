8b25120fcc1a0b306d8647a69afedb99fa61d48d
aghe12 <meghashettyj@gmail.com>
2026-04-10 10:41:50 +0000

    created SQL and PHP for storing data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { openInteractionManager } from "./presentation/interaction-manager.js"
 import { manageFriends } from "./presentation/friends-manager.js"
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 
  const run = async ()=>{
     const {close,choose,ask} = openInteractionManager();
@@ -9,73 +9,73 @@ import mysql from 'mysql2/promise';
 
 run().catch(console.error);
 
-function consoleDir(param: any) {
-  console.dir(param, {
-    showHidden: false,
-    depth: null,
-    colors: true,
-  });
-}
+// function consoleDir(param: any) {
+//   console.dir(param, {
+//     showHidden: false,
+//     depth: null,
+//     colors: true,
+//   });
+// }
 
-async function runQuery(
-  connection: mysql.Connection,
-  label: string,
-  query: string,
-) {
-  try {
-    console.log(`===Running ${label}===\n`);
-    const [results] = await connection.query(query);
-    consoleDir(results);
-    console.log('\n\n\n');
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function runQuery(
+//   connection: mysql.Connection,
+//   label: string,
+//   query: string,
+// ) {
+//   try {
+//     console.log(`===Running ${label}===\n`);
+//     const [results] = await connection.query(query);
+//     consoleDir(results);
+//     console.log('\n\n\n');
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-const tableName = '`friends`';
+// const tableName = '`friends`';
 
-// Select query
-const selectQuery = `SELECT * FROM ${tableName}`;
-// INSERT
-const insertQuery = `INSERT INTO ${tableName} (
-    \`id\`, 
-    \`name\`, 
-    \`email\`, 
-     \`phone\`) VALUES (
-     NULL, 
-    'Himanshu', 
-    'himanshu@gmail.com', 
-    '9448263380');
-    `;
+// // Select query
+// const selectQuery = `SELECT * FROM ${tableName}`;
+// // INSERT
+// const insertQuery = `INSERT INTO ${tableName} (
+//     \`id\`, 
+//     \`name\`, 
+//     \`email\`, 
+//      \`phone\`) VALUES (
+//      NULL, 
+//     'Himanshu', 
+//     'himanshu@gmail.com', 
+//     '9448263380');
+//     `;
 
-// UPDATE
-const updateQuery = `UPDATE 
-${tableName}
-SET \`email\` = 'shrijith1234@gmail.com' 
-WHERE \`id\` = 2`;
+// // UPDATE
+// const updateQuery = `UPDATE 
+// ${tableName}
+// SET \`email\` = 'shrijith1234@gmail.com' 
+// WHERE \`id\` = 2`;
 
-// DELETE
-const deleteQuery = `DELETE FROM ${tableName} WHERE \`id\` = 3`;
+// // DELETE
+// const deleteQuery = `DELETE FROM ${tableName} WHERE \`id\` = 3`;
 
-async function runAllQueries() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST ?? '',
-    user: process.env.DB_USER ?? '',
-    password: process.env.DB_PASSWORD ?? '',
-    database: process.env.DB_NAME ?? '',
-  });
-  await runQuery(connection, 'SELECT', selectQuery);
+// async function runAllQueries() {
+//   const connection = await mysql.createConnection({
+//     host: process.env.DB_HOST ?? '',
+//     user: process.env.DB_USER ?? '',
+//     password: process.env.DB_PASSWORD ?? '',
+//     database: process.env.DB_NAME ?? '',
+//   });
+//   await runQuery(connection, 'SELECT', selectQuery);
 
-  await runQuery(connection, 'INSERT', insertQuery);
-  await runQuery(connection, 'SELECT', selectQuery);
+//   await runQuery(connection, 'INSERT', insertQuery);
+//   await runQuery(connection, 'SELECT', selectQuery);
 
-  await runQuery(connection, 'UPDATE', updateQuery);
-  await runQuery(connection, 'SELECT', selectQuery);
+//   await runQuery(connection, 'UPDATE', updateQuery);
+//   await runQuery(connection, 'SELECT', selectQuery);
 
-  await runQuery(connection, 'DELETE', deleteQuery);
-  await runQuery(connection, 'SELECT', selectQuery);
+//   await runQuery(connection, 'DELETE', deleteQuery);
+//   await runQuery(connection, 'SELECT', selectQuery);
 
-  await connection.end();
-}
+//   await connection.end();
+// }
 
-await runAllQueries();
+// await runAllQueries();
